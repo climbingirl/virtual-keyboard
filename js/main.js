9a63006457aab77d9f9ff62a6914cc0db0913af9
climbingirl <isvetlanaivanova@yandex.ru>
2022-05-10 01:56:27 +0700

    feat: implement switcing language

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const KEYS_EN = [
+const KEYS_EN_ARR = [
     [
         {"code": "Backquote", "caseDown": "`", "caseUp": "~", "caps": "`", "shiftCaps": "~"},
         {"code": "Digit1", "caseDown": "1", "caseUp": "!", "caps": "1", "shiftCaps": "!"},
@@ -66,6 +66,83 @@ const KEYS_EN = [
     ],
     [
         {"code": "ControlLeft", "caseDown": "Ctrl", "caseUp": "Ctrl", "caps": "Ctrl", "shiftCaps": "Ctrl"},
+        {"code": "MetaLeft", "caseDown": "Win", "caseUp": "Win", "caps": "Win", "shiftCaps": "Win"},
+        {"code": "AltLeft", "caseDown": "Alt", "caseUp": "Alt", "caps": "Alt", "shiftCaps": "Alt"},
+        {"code": "Space", "caseDown": "", "caseUp": "", "caps": "", "shiftCaps": ""},
+        {"code": "AltRight", "caseDown": "Alt", "caseUp": "Alt", "caps": "Alt", "shiftCaps": "Alt"},
+        {"code": "ArrowLeft", "caseDown": "&#9668;", "caseUp": "&#9668;", "caps": "&#9668;", "shiftCaps": "&#9668;"},
+        {"code": "ArrowDown", "caseDown": "&#9660;", "caseUp": "&#9660;", "caps": "&#9660;", "shiftCaps": "&#9660;"},
+        {"code": "ArrowRight", "caseDown": "&#9658;", "caseUp": "&#9658;", "caps": "&#9658;", "shiftCaps": "&#9658;"},
+        {"code": "ControlRight", "caseDown": "Ctrl", "caseUp": "Ctrl", "caps": "Ctrl", "shiftCaps": "Ctrl"}
+    ]
+];
+
+const KEYS_RU_ARR = [
+    [
+        {"code": "Backquote", "caseDown": "ё", "caseUp": "Ё", "caps": "Ё", "shiftCaps": "ё"},
+        {"code": "Digit1", "caseDown": "1", "caseUp": "!", "caps": "1", "shiftCaps": "!"},
+        {"code": "Digit2", "caseDown": "2", "caseUp": "\"", "caps": "2", "shiftCaps": "\""},
+        {"code": "Digit3", "caseDown": "3", "caseUp": "№", "caps": "3", "shiftCaps": "№"},
+        {"code": "Digit4", "caseDown": "4", "caseUp": ";", "caps": "4", "shiftCaps": ";"},
+        {"code": "Digit5", "caseDown": "5", "caseUp": "%", "caps": "5", "shiftCaps": "%"},
+        {"code": "Digit6", "caseDown": "6", "caseUp": ":", "caps": "6", "shiftCaps": ":"},
+        {"code": "Digit7", "caseDown": "7", "caseUp": "?", "caps": "7", "shiftCaps": "?"},
+        {"code": "Digit8", "caseDown": "8", "caseUp": "*", "caps": "8", "shiftCaps": "*"},
+        {"code": "Digit9", "caseDown": "9", "caseUp": "(", "caps": "9", "shiftCaps": "("},
+        {"code": "Digit0", "caseDown": "0", "caseUp": ")", "caps": "0", "shiftCaps": ")"},
+        {"code": "Minus", "caseDown": "-", "caseUp": "_", "caps": "-", "shiftCaps": "_"},
+        {"code": "Equal", "caseDown": "=", "caseUp": "+", "caps": "=", "shiftCaps": "+"},
+        {"code": "Backspace", "caseDown": "Backspace", "caseUp": "Backspace", "caps": "Backspace", "shiftCaps": "Backspace"}
+    ],
+    [
+        {"code": "Tab", "caseDown": "Tab", "caseUp": "Tab", "caps": "Tab", "shiftCaps": "Tab"},
+        {"code": "KeyQ", "caseDown": "й", "caseUp": "Й", "caps": "й", "shiftCaps": "Й"},
+        {"code": "KeyW", "caseDown": "ц", "caseUp": "Ц", "caps": "ц", "shiftCaps": "Ц"},
+        {"code": "KeyE", "caseDown": "у", "caseUp": "У", "caps": "у", "shiftCaps": "У"},
+        {"code": "KeyR", "caseDown": "к", "caseUp": "К", "caps": "к", "shiftCaps": "К"},
+        {"code": "KeyT", "caseDown": "е", "caseUp": "Е", "caps": "е", "shiftCaps": "Е"},
+        {"code": "KeyY", "caseDown": "н", "caseUp": "Н", "caps": "н", "shiftCaps": "Н"},
+        {"code": "KeyU", "caseDown": "г", "caseUp": "Г", "caps": "г", "shiftCaps": "Г"},
+        {"code": "KeyI", "caseDown": "ш", "caseUp": "Ш", "caps": "ш", "shiftCaps": "Ш"},
+        {"code": "KeyO", "caseDown": "щ", "caseUp": "Щ", "caps": "щ", "shiftCaps": "Щ"},
+        {"code": "KeyP", "caseDown": "з", "caseUp": "З", "caps": "з", "shiftCaps": "З"},
+        {"code": "BracketLeft", "caseDown": "х", "caseUp": "Х", "caps": "х", "shiftCaps": "Х"},
+        {"code": "BracketRight", "caseDown": "ъ", "caseUp": "Ъ", "caps": "ъ", "shiftCaps": "Ъ"},
+        {"code": "Backslash", "caseDown": "\\", "caseUp": "/", "caps": "\\", "shiftCaps": "/"},
+        {"code": "Delete", "caseDown": "Delete", "caseUp": "Delete", "caps": "Delete", "shiftCaps": "Delete"}
+    ],
+    [
+        {"code": "CapsLock", "caseDown": "CapsLock", "caseUp": "CapsLock", "caps": "CapsLock", "shiftCaps": "CapsLock"},
+        {"code": "KeyA", "caseDown": "ф", "caseUp": "Ф", "caps": "ф", "shiftCaps": "Ф"},
+        {"code": "KeyS", "caseDown": "ы", "caseUp": "Ы", "caps": "ы", "shiftCaps": "Ы"},
+        {"code": "KeyD", "caseDown": "в", "caseUp": "В", "caps": "в", "shiftCaps": "В"},
+        {"code": "KeyF", "caseDown": "а", "caseUp": "А", "caps": "а", "shiftCaps": "А"},
+        {"code": "KeyG", "caseDown": "п", "caseUp": "П", "caps": "п", "shiftCaps": "П"},
+        {"code": "KeyH", "caseDown": "р", "caseUp": "Р", "caps": "р", "shiftCaps": "Р"},
+        {"code": "KeyJ", "caseDown": "о", "caseUp": "О", "caps": "о", "shiftCaps": "О"},
+        {"code": "KeyK", "caseDown": "л", "caseUp": "Л", "caps": "л", "shiftCaps": "Л"},
+        {"code": "KeyL", "caseDown": "д", "caseUp": "Д", "caps": "д", "shiftCaps": "Д"},
+        {"code": "Semicolon", "caseDown": "ж", "caseUp": "Ж", "caps": "ж", "shiftCaps": "Ж"},
+        {"code": "Quote", "caseDown": "э", "caseUp": "Э", "caps": "э", "shiftCaps": "Э"},
+        {"code": "Enter", "caseDown": "Enter", "caseUp": "Enter", "caps": "Enter", "shiftCaps": "Enter"}
+    ],
+    [
+        {"code": "ShiftLeft", "caseDown": "Shift", "caseUp": "Shift", "caps": "Shift", "shiftCaps": "Shift"},
+        {"code": "KeyZ", "caseDown": "я", "caseUp": "Я", "caps": "я", "shiftCaps": "Я"},
+        {"code": "KeyX", "caseDown": "ч", "caseUp": "Ч", "caps": "ч", "shiftCaps": "Ч"},
+        {"code": "KeyC", "caseDown": "с", "caseUp": "С", "caps": "с", "shiftCaps": "С"},
+        {"code": "KeyV", "caseDown": "м", "caseUp": "М", "caps": "м", "shiftCaps": "М"},
+        {"code": "KeyB", "caseDown": "и", "caseUp": "И", "caps": "и", "shiftCaps": "И"},
+        {"code": "KeyN", "caseDown": "т", "caseUp": "Т", "caps": "т", "shiftCaps": "Т"},
+        {"code": "KeyM", "caseDown": "ь", "caseUp": "Ь", "caps": "ь", "shiftCaps": "Ь"},
+        {"code": "Comma", "caseDown": "б", "caseUp": "Б", "caps": "б", "shiftCaps": "Б"},
+        {"code": "Period", "caseDown": "ю", "caseUp": "Ю", "caps": "ю", "shiftCaps": "Ю"},
+        {"code": "Slash", "caseDown": ".", "caseUp": ",", "caps": ".", "shiftCaps": ","},
+        {"code": "ArrowUp", "caseDown": "&#9650;", "caseUp": "&#9650;", "caps": "&#9650;", "shiftCaps": "&#9650;"},
+        {"code": "ShiftRight", "caseDown": "Shift", "caseUp": "Shift", "caps": "Shift", "shiftCaps": "Shift"}
+    ],
+    [
+        {"code": "ControlLeft", "caseDown": "Ctrl", "caseUp": "Ctrl", "caps": "Ctrl", "shiftCaps": "Ctrl"},
         {"code": "AltLeft", "caseDown": "Alt", "caseUp": "Alt", "caps": "Alt", "shiftCaps": "Alt"},
         {"code": "MetaLeft", "caseDown": "Win", "caseUp": "Win", "caps": "Win", "shiftCaps": "Win"},
         {"code": "Space", "caseDown": "", "caseUp": "", "caps": "", "shiftCaps": ""},
@@ -92,11 +169,11 @@ function renderApp() {
 
     const KEYBOARD = document.createElement("div");
     KEYBOARD.classList.add("keyboard");
-    KEYBOARD.append(createKeys(KEYS_EN, "en"))
+    KEYBOARD.append(createKeys(KEYS_EN_ARR))
     CONTAINER.append(KEYBOARD);
 }
 
-function createKeys(keys, lang) {
+function createKeys(keys) {
     const KEYBOARD_INNER = document.createElement("div");
     KEYBOARD_INNER.classList.add("keyboard__inner");
 
@@ -142,11 +219,17 @@ function createKeys(keys, lang) {
             }
 
             KEY.innerHTML = `
-                <span class="key__language language__${lang}">
+                <span class="key__language language__en">
                     <span class="caseDown">${currentKey.caseDown}</span>
                     <span class="caseUp hidden">${currentKey.caseUp}</span>
                     <span class="caps hidden">${currentKey.caps}</span>
                     <span class="shiftCaps hidden">${currentKey.shiftCaps}</span>
+                </span>
+                <span class="key__language language__ru hidden">
+                    <span class="caseDown">${KEYS_RU_ARR[i][j].caseDown}</span>
+                    <span class="caseUp hidden">${KEYS_RU_ARR[i][j].caseUp}</span>
+                    <span class="caps hidden">${KEYS_RU_ARR[i][j].caps}</span>
+                    <span class="shiftCaps hidden">${KEYS_RU_ARR[i][j].shiftCaps}</span>
                 </span>
             `;
             
@@ -164,6 +247,8 @@ renderApp();
 // add keyboard events
 
 const KEYS = document.querySelectorAll(".key");
+const KEYS_EN = document.querySelectorAll(".language__en");
+const KEYS_RU = document.querySelectorAll(".language__ru");
 const KEYS_UP = document.querySelectorAll(".caseUp");
 const KEYS_DOWN = document.querySelectorAll(".caseDown");
 const KEYS_CAPS = document.querySelectorAll(".caps");
@@ -186,6 +271,11 @@ window.addEventListener("keydown", function (e) {
         if(e.key === "Shift") {
             KEYS_DOWN[i].classList.add("hidden");
             KEYS_UP[i].classList.remove("hidden");
+        }
+
+        if(e.ctrlKey && e.code == "AltLeft" ) {
+            KEYS_EN[i].classList.toggle("hidden");
+            KEYS_RU[i].classList.toggle("hidden");
         }
     }
 
